8fc5c2914d5cc402d4c2dfb87e611187ecc68915
Luis Bravo <ledbrv@gmail.com>
2022-11-24 11:09:59 -0300

    Stopped at video 41

---
+++ structs/go.mod
@@ -0,0 +1,3 @@
+module go-the-complete-developers-guide/structs
+
+go 1.19
---
+++ structs/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contact   contactInfo
+}
+
+func main() {
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contact: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 9400,
+		},
+	}
+	fmt.Printf("%+v\n", jim)
+}
